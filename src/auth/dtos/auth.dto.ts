09b81911f4eac9415b83ece6225b9b18102a7376
Ansociuu <147503704+Ansociuu@users.noreply.github.com>
2025-06-14 04:53:52 +0000

    Auth-jwt

---
+++ src/auth/dtos/auth.dto.ts
@@ -0,0 +1,23 @@
+import {IsEmail, IsNotEmpty, MinLength} from 'class-validator'
+
+export class RegisterDto {
+    @IsEmail()
+    email: string          
+    
+    @MinLength(6)
+    password: string
+    
+    @IsNotEmpty()
+    name: string
+    
+    language: string
+}
+
+export class LoginDto {
+    @IsEmail()
+    email: string;
+
+    @IsNotEmpty()
+    @MinLength(6)
+    password: string
+}
